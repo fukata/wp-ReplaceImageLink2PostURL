20bbc6915e32a138128dfab5acd862480306f8f0
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2013-02-08 21:47:38 +0900

    基本動作まで実装

--- ReplaceImageLink2PostURL.php
+++ ReplaceImageLink2PostURL.php
@@ -9,12 +9,29 @@ Author URI: http://fukata.org
 */
 
 function ril_replace2post_url( $content='' ) {
+    global $post;
+
+    $urls = array(
+        'flickr.com',
+    );
+    $post_url = get_permalink($post->ID);
+
+    foreach ( $urls as $url ) {
+        $pattern = '/(<a\shref=")[^"]*' . $url . '[^"]*("[^>]*><img\ssrc="[^"]+"[^\/]*\/><\/a>)/i';
+        $match_count = 0;
+        $content = preg_replace( $pattern, '${1}' . $post_url . '${2}', $content, -1, &$match_count );
+        if ( $match_count > 0 ) break;
+    }
+
+    return $content;
 }
 
 function ril_filter_content_replace2post_url( $content ) {
-    if ( is_single() ) return $content;
-
-    return $content;
+    if ( is_single() ) {
+        return $content;
+    } else {
+        return ril_replace2post_url( $content );
+    }
 }
 
 add_filter( 'the_content', 'ril_filter_content_replace2post_url' );
